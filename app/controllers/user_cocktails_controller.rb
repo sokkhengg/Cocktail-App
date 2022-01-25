59cc6d6a696c9fa767832a80626ec9f5d0acd22c
smh1988-2 <seamhurley1988@gmail.com>
2022-01-24 18:07:50 -0800

    backend method for user cocktails

--- app/controllers/user_cocktails_controller.rb
+++ app/controllers/user_cocktails_controller.rb
@@ -4,4 +4,22 @@ class UserCocktailsController < ApplicationController
         render json: UserCocktail.all
     end
 
+    def user_cocktails_all
+        user = User.find(params[:id]) #find current user
+        cocktails = []
+
+        # # loop through all Cocktail and checking the ingredient
+        Cocktail.all.each do |c|
+            ctail = c.ingredients.all? do |i|
+                user.ingredients.include?(i)
+            end
+            if ctail 
+                cocktails.push(c)
+            end
+        end
+        
+        render json: cocktails
+    end
+
+
 end
